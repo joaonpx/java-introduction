aabada8b1bf59b2b8f7a15a6b337be78d174bdad
Npx <cheyprivado@gmail.com>
2024-04-08 13:10:31 -0300

    add arrays & casting examples

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.ArrayList;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    /* common array */
+    int[] arr = new int[10];
+
+    /* dynamic array */
+    ArrayList<String> fruits = new ArrayList<String>();
+    fruits.add("abacaxi");
+    fruits.add("banana");
+    fruits.add("maça");
+    fruits.forEach(System.out::println);
+
+    /* casting int to string */
+    int id = 31854879;
+    String idStr;
+    idStr = String.valueOf(id);
+
+   System.out.println(idStr + idStr.getClass().getName());
   }
 }
